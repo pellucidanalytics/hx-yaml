595a0a0314659b2ac116afee83f86ea91bcd9a4e
Franco Ponticelli <franco.ponticelli@gmail.com>
2019-02-10 21:21:42 -0700

    update getter syntax

--- src/yaml/YamlException.hx
+++ src/yaml/YamlException.hx
@@ -12,7 +12,7 @@ class YamlException
 
 	public var cause(default, null):Dynamic;
 	public var info(default, null):PosInfos;
-	
+
     public function new(?message:String="", ?cause:Dynamic = null, ?info:PosInfos)
 	{
 		this.name = Type.getClassName(Type.getClass(this));
